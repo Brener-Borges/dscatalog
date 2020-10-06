eb91c2738e96bc5d59169f219554d81803d0a881
Brener Pereira Borges <brenerborges@gmail.com>
2020-10-06 15:15:45 -0300

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,12 @@
 INSERT INTO tb_category (name, created_At) VALUES ('Books', NOW());
 INSERT INTO tb_category (name, created_At) VALUES ('Electronics', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Computers', NOW());  
+INSERT INTO tb_category (name, created_At) VALUES ('Computers', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Clothes', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Toys', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Garden', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Office', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('School', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Bath', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Food', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Video Games', NOW());
+  
